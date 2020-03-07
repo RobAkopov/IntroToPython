cc02429a684b56ed31153c97536e336eea029d44
robakopov <robakopov@gmail.com>
2020-03-08 01:48:52 +0400

    08.03.2020

---
+++ Week1/Practical/Problem1.py
@@ -0,0 +1,23 @@
+var_project = "cake"
+var_difficulty = 3
+var_ingredients = ["flour", "butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+
+print("apples" in var_ingredients)
+print("butter" in var_ingredients)
+print(("eggs" in var_ingredients) or ("margarine" in var_ingredients))
+print(("eggs" in var_ingredients) and ("margarine" in var_ingredients))
+
+
+var_flour = 175
+var_butter = 175
+var_sugar = "100g"
+var_eggs = 2
+var_cocoa_powder = "1ts"
+var_baking_powder = 0.5
+
+type(var_flour)
+type(var_butter)
+type(var_sugar)
+type(var_eggs)
+type(var_cocoa_powder)
+type(var_baking_powder)
