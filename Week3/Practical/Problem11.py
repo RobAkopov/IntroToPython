41c340264baf4ebb571ca06a2a96ed03ffa99872
RobAkopov <61898452+RobAkopov@users.noreply.github.com>
2020-03-29 00:28:17 +0400

    Update Problem11.py

--- Week3/Practical/Problem11.py
+++ Week3/Practical/Problem11.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 set1 = {1, 2, 3, 'a'}
 set2 = {'a', 'b', 'c'}
 print(set1.union(set2))
-=======
 set1 = {1, 2, 3, 'a'}
 set2 = {'a', 'b', 'c'}
 print(set1.union(set2))
->>>>>>> 9ac430454f7e3209c56ca3244d8b7e5377a24518
 print(set1.intersection(set2))
