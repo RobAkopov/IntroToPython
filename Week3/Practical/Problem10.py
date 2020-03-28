7ee6cbb98cd44d27b2487e5ae1475d1acf1fa66a
RobAkopov <61898452+RobAkopov@users.noreply.github.com>
2020-03-29 00:27:36 +0400

    Update Problem10.py

--- Week3/Practical/Problem10.py
+++ Week3/Practical/Problem10.py
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 set2 = {'a', 1, 'b', 2, 'c', 3}
 x = input("Input item: ")
 print(set2)
 set2.discard(x)
-=======
 set2 = {'a', 1, 'b', 2, 'c', 3}
 x = input("Input item: ")
 print(set2)
 set2.discard(x)
->>>>>>> 9ac430454f7e3209c56ca3244d8b7e5377a24518
 print(set2)
