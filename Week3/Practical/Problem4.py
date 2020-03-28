000164b9642495940e100aa0ab6baa62aeef4a75
RobAkopov <61898452+RobAkopov@users.noreply.github.com>
2020-03-29 00:21:26 +0400

    Update Problem4.py

--- Week3/Practical/Problem4.py
+++ Week3/Practical/Problem4.py
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 list4 = ['a', 1, 'b', 2, 'c', 3]
 list4.append(input('Write anything: '))
 print(list4)
 list4.pop(0)
-=======
 list4 = [1,2,3,4,5,'hello','a',7,8,6,'b']
 list4.append(input('Write anything: '))
 print(list4)
 list4.pop(0)
->>>>>>> 9ac430454f7e3209c56ca3244d8b7e5377a24518
 print(list4)
