8439c45f1f1553dbbfe5702826717d448abd6fcb
RobAkopov <61898452+RobAkopov@users.noreply.github.com>
2020-03-29 01:15:12 +0400

    Update Problem6.py

--- Week3/Practical/Problem6.py
+++ Week3/Practical/Problem6.py
@@ -4,10 +4,4 @@ list6 = list5.copy()
 del list6[5]
 del list6[4]
 del list6[0]
-list5 = [1,2,3,4,5,'hello','a',7,8,6,'b']
-print(list5)
-list6 = list5.copy()
-del list6[5]
-del list6[4]
-del list6[0]
 print(list6)
