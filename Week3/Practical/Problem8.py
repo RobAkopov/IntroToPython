ad14404fb1df7a69bf60a47eb78b38a3197c499d
RobAkopov <61898452+RobAkopov@users.noreply.github.com>
2020-03-29 00:25:53 +0400

    Update Problem8.py

--- Week3/Practical/Problem8.py
+++ Week3/Practical/Problem8.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 set1 = {'a', 1, 'b', 2, 'c', 3}
 print(set1)
 set1.add(input("Input item: "))
-=======
 set1 = {'a', 1, 'b', 2, 'c', 3}
 print(set1)
 set1.add(input("Input item: "))
->>>>>>> 9ac430454f7e3209c56ca3244d8b7e5377a24518
 print(set1)
