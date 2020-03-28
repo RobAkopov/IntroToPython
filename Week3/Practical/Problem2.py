8bae3970e416bd56ab60743069402cdfb2c44cc3
RobAkopov <61898452+RobAkopov@users.noreply.github.com>
2020-03-29 00:20:12 +0400

    Update Problem2.py

--- Week3/Practical/Problem2.py
+++ Week3/Practical/Problem2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 list1 = ['hello', 1, True]
 print(list1)
 list2 = list1.copy()
@@ -6,14 +5,11 @@ list2.append(input('Write anything: '))
 list2.append(input('Write anything: '))
 list2.append(input('Write anything: '))
 print(list2)
-=======
 list1 = ['hello', 1, True]
 print(list1)
 list2 = list1.copy()
 list2.append(input('Write anything: '))
 list2.append(input('Write anything: '))
 list2.append(input('Write anything: '))
-
 print(list2)
->>>>>>> 9ac430454f7e3209c56ca3244d8b7e5377a24518
 print(list1)
