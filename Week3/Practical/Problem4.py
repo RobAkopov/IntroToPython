1316b6021e03099bf7199d137790daf08aca9cc7
RobAkopov <61898452+RobAkopov@users.noreply.github.com>
2020-03-29 01:10:25 +0400

    Update Problem4.py

--- Week3/Practical/Problem4.py
+++ Week3/Practical/Problem4.py
@@ -1,9 +1,6 @@
 list4 = ['a', 1, 'b', 2, 'c', 3]
-list4.append(input('Write anything: '))
+x = (input('Write anything: '))
+list4.append(x)
 print(list4)
-list4.pop(0)
-list4 = [1,2,3,4,5,'hello','a',7,8,6,'b']
-list4.append(input('Write anything: '))
-print(list4)
-list4.pop(0)
+list4.remove(x)
 print(list4)
