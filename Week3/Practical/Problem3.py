f167f5a1ba01704b7e466e034fc96622d36ebb95
RobAkopov <61898452+RobAkopov@users.noreply.github.com>
2020-03-29 00:20:55 +0400

    Update Problem3.py

--- Week3/Practical/Problem3.py
+++ Week3/Practical/Problem3.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 list2 = ['a', 1, 'b', 2, 'c', 3]
 y = int(input('Write a number: '))
 z = input('Write letters: ')
@@ -6,7 +5,6 @@ list2.append(y)
 list2.append(z)
 print('list2:', list2)
 print('count of numbers', y, ':', list2.count(y))
-=======
 list2 = ['a', 1, 'b', 2, 'c', 3]
 y = int(input('Write a number: '))
 z = input('Write letters: ')
@@ -14,5 +12,4 @@ list2.append(y)
 list2.append(z)
 print('list2:', list2)
 print('count of numbers', y, ':', list2.count(y))
->>>>>>> 9ac430454f7e3209c56ca3244d8b7e5377a24518
 print('count of letters', z, ':', list2.count(z))
