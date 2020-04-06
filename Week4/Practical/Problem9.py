6caae3b3d69899ebb095fdd957951cb4a2fb31f0
Rob Akopov <robakopov@gmail.com>
2020-04-06 19:07:48 +0400

    adding

--- Week4/Practical/Problem9.py
+++ Week4/Practical/Problem9.py
@@ -1,8 +1,10 @@
 correct_num = 5
-guess = int(input('Input an integer: '))
-for n in range(10):
+guess = int(input('Input a integer: '))
+for n in range(11):
     if guess == correct_num:
         print('That was a good guess!')
+        break
+    elif n == 10:
         break
     elif n == 8:
         print('Your Last Chance')
